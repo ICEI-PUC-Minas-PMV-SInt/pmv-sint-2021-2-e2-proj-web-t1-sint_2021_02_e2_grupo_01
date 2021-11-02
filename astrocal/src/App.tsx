cf97b9612f32d1dd04191350dbf2c5fd90ecd0b3
alcpl01 <lindinaluli12@gmail.com>
2021-11-02 00:14:00 -0300

    Site com React (Menu)

---
+++ astrocal/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
